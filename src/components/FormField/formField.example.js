b5a721f69a4b3713d76b1d19c43ba3bb20ea84bf
Eric Shelley <eric@webdesignbakery.com>
2019-10-11 14:01:51 -0600

    Wrap react-final-form in <Suspense>
    * Fixing render issue with react-final-form where Field component was suspending rendering

--- src/components/FormField/formField.example.js
+++ src/components/FormField/formField.example.js
@@ -1,5 +1,7 @@
 export const initialValues = {
   color: '096dd9',
+  daysOfEatingKeto: 25,
+  phoneNumber: '(828) 281-8121',
   name: 'Eric Shelley',
-  message: 'Barry is a dork!',
+  message: 'Ham and cheese sounds good right now.',
 }
