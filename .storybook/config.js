691476b744384317f2120aeb66d26cd0de28a64b
Eric Shelley <eric@webdesignbakery.com>
2019-10-11 15:46:03 -0600

    Adding decorator to all stories
    This adds padding around the color picker so that the popover will render in canvas

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,8 @@
-import { configure } from '@storybook/react'
+import React from 'react'
+import { addDecorator, configure } from '@storybook/react'
+// import { DocsPage, DocsContainer } from '@storybook/addon-docs/blocks'
+
+addDecorator(storyFn => <div style={{ padding: '2em' }}>{storyFn()}</div>)
 
 // automatically import all files ending in *.stories.js
 // configure(require.context("../stories", true, /\.stories\.(js|mdx)$/), module);
